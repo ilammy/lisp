6d4803e055a5c40407e1c43fa1682a62c0d821e6
ilammy <me@ilammy.net>
2021-10-24 17:11:57 +0900

    pollen: Chapter 4: section 4.3.14

---
+++ book-src/ch04-assignment-and-side-effects/03.14-starting-the-interpreter.poly.pm
@@ -0,0 +1,42 @@
+#lang pollen
+
+◊subsection[#:label "assignment/implementation/ssect:starting"]{Запускаем интерпретатор}
+
+Наконец, всё готово для запуска нашего интерпретатора.
+Главный цикл вызывает ◊ic{evaluate} с~рекурсивным продолжением,
+которое обеспечивает непрерывность сознания интерпретатора,
+передавая следующему вызову память предыдущего.
+
+◊indexC{chapter4-interpreter}
+◊code:lisp{
+(define (chapter4-interpreter)
+  (define (toplevel s)
+    (evaluate (read) r.global s
+              (lambda (v ss)
+                (display (transcode-back v ss))
+                (toplevel ss) ) ) )
+  (toplevel s.global) )
+}
+
+◊indexR{представление!значений}
+Главной особенностью этого интерпретатора является~то,
+что представление данных интерпретируемого языка не~совпадает с~представлением данных в~языке реализации.
+Особенно разительно отличаются точечные пары и, следовательно, списки.
+Поэтому мы вынуждены воспользоваться функцией ◊ic{transcode-back}
+для преобразования результата вычислений в~нечто понятное ◊ic{display},
+чтобы его можно было вывести на~экран.
+Эту~вспомогательную функцию вполне можно сразу объединить с~примитивом ◊ic{display}.
+
+◊indexC{transcode-back}
+◊code:lisp{
+(define (transcode-back v s)
+  (case (v 'type)
+    ((null)     '())
+    ((boolean)  ((v 'boolify) #t #f))
+    ((symbol)   (v 'name))
+    ((number)   (v 'value))
+    ((pair)     (cons (transcode-back (s (v 'car)) s)
+                      (transcode-back (s (v 'cdr)) s) ))
+    ((function) v)  ; есть ли другие варианты?
+    (else       (wrong "Unknown type" (v 'type))) ) )
+}
--- book-src/ch04_assignment_and_side_effects.poly.pm
+++ book-src/ch04_assignment_and_side_effects.poly.pm
@@ -1,49 +1,5 @@
 % -*- coding: utf-8 -*-
 
-◊subsection{Запускаем интерпретатор}%
-◊label{assignment/implementation/ssect:starting}
-
-Наконец, всё готово для запуска нашего интерпретатора.
-Главный цикл вызывает
-◊ic{evaluate} с~рекурсивным продолжением, которое обеспечивает непрерывность
-сознания интерпретатора, передавая следующему вызову память предыдущего.
-
-◊indexC{chapter4-interpreter}
-◊code:lisp{
-(define (chapter4-interpreter)
-  (define (toplevel s)
-    (evaluate (read) r.global s
-              (lambda (v ss)
-                (display (transcode-back v ss))
-                (toplevel ss) ) ) )
-  (toplevel s.global) )
-}
-
-◊indexR{представление!значений}
-Главной особенностью данного интерпретатора является отличие представления
-данных интерпретируемого языка от представления данных в~языке реализации.
-Особенно разительно отличаются точечные пары и, следовательно, списки, поэтому
-мы вынуждены воспользоваться функцией ◊ic{transcode-back} для преобразования
-результата вычислений в~нечто понятное ◊ic{display}, чтобы его можно было
-вывести на экран.
-Эту вспомогательную функцию вполне можно сразу объединить
-с~примитивом ◊ic{display}.
-
-◊indexC{transcode-back}
-◊code:lisp{
-(define (transcode-back v s)
-  (case (v 'type)
-    ((null)     '())
-    ((boolean)  ((v 'boolify) #t #f))
-    ((symbol)   (v 'name))
-    ((number)   (v 'value))
-    ((pair)     (cons (transcode-back (s (v 'car)) s)
-                      (transcode-back (s (v 'cdr)) s) ))
-    ((function) v)  ; почему бы и нет?
-    (else       (wrong "Unknown type" (v 'type))) ) )
-}
-
-
 ◊section[#:label "assignment/sect:io-and-memory"]{Ввод-вывод и~память}
 
 ◊indexR{потоки ввода-вывода}
